faacc060411c40e6360a9eb7e771339af89e7c34
gre1pk <raik1996@gmail.com>
2022-05-06 13:25:51 +0300

    feat: add action Tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,19 +1,44 @@
+import { useDispatch, useSelector } from 'react-redux'
+
+import select from '../../store/actions/tabsActions'
+
 import classes from './Tabs.module.scss'
 
 function Tabs() {
-  return (
-    <div className={classes.tabs}>
-      <button className={`${classes.active} ${classes.btn} ${classes['first-el']}`} type="button">
-        Самый дешевый
+  const filtersBtn = [
+    {
+      name: 'Самый дешевый',
+      value: 'cheap',
+    },
+    {
+      name: 'Самый быстрый',
+      value: 'fast',
+    },
+    {
+      name: 'Оптимальный',
+      value: 'optimal',
+    },
+  ]
+
+  const { ticketSort } = useSelector((state) => state.tabsReducer)
+  const dispatch = useDispatch()
+
+  const onSelect = (value) => dispatch(select(value))
+
+  const btnList = filtersBtn.map((e) => {
+    let classValue = classes.btn
+    if (ticketSort === e.value) {
+      classValue += ` ${classes.active}`
+    }
+
+    return (
+      <button className={classValue} type="button" key={e.value} onClick={() => onSelect(e.value)}>
+        {e.name}
       </button>
-      <button className={classes.btn} type="button">
-        Самый быстрый
-      </button>
-      <button className={`${classes.btn} ${classes['last-el']}`} type="button">
-        Оптимальный
-      </button>
-    </div>
-  )
+    )
+  })
+
+  return <div className={classes.tabs}>{btnList}</div>
 }
 
 export default Tabs
